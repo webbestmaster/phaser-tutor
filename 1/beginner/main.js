4c976ce71174ebf5941f1cf2319770fdec0e96f5
webbestmaster <web.best.master@gmail.com>
2016-08-10 00:49:04 +0300

    1 - fix

--- 1/beginner/main.js
+++ 1/beginner/main.js
@@ -160,10 +160,9 @@ function createEnemies() {
 	enemies.y = 50;
 
 
-	var tween = game.add.tween(enemies).to({x: 200}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+	var tween = game.add.tween(enemies).to({x: 200}, 2000, Phaser.Easing.Linear.None, true, 0, Infinity, true);
 
-
-	tween.onLoop.add(descend, this);
+	tween.onRepeat.add(descend, null);
 
 }
 
